937d5b7765d6e9fe9ed79a22c3a6b0e4dadff88b
Shahid Ansari <shahidansari2068@gmail.com>
2025-09-27 04:16:18 +0530

    main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def load_order_items():
                 on_bad_lines='skip',
                 skip_blank_lines=True
             )
-        except Exception as e:
+          except Exception as e:
             st.error(f"Error reading order_items.csv: {e}")
             return pd.DataFrame(columns=["OrderID", "Item", "Qty", "UnitPrice", "ItemTotal"])
         return pd.read_csv(ORDER_ITEMS_FILE)
